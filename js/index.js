2b6aec3c2a23c7064d515cef71ec9f29b7d46319
Mikhail <schaslivchikm@gmail.com>
2022-02-07 23:09:00 +0200

    homve work nasledovanie

--- js/index.js
+++ js/index.js
@@ -8,21 +8,45 @@ class Vehicle {
     this.manufactureDate = manufactureDate;
   }
   getMaxSize() {
-    for(let value in this.dimensions){
-      if(Number.isNaN(this.dimensions[value]) || !Number.isInteger(this.dimensions[value]) || typeof this.dimensions[value] !== 'number'){
-        throw new TypeError('must be Integer Number')
+    for (let value in this.dimensions) {
+      if (
+        Number.isNaN(this.dimensions[value]) ||
+        !Number.isInteger(this.dimensions[value]) ||
+        typeof this.dimensions[value] !== "number"
+      ) {
+        throw new TypeError("must be Integer Number");
       }
     }
-    for(let value in this.dimensions){
-      if(this.dimensions[value] < 0){
-        throw new TypeError('value must be over 0mm')
+    for (let value in this.dimensions) {
+      if (this.dimensions[value] < 0) {
+        throw new TypeError("value must be over 0mm");
       }
     }
-     const { length, width, height } = this.dimensions;
+    const { length, width, height } = this.dimensions;
     return `length: ${length}mm,  width: ${width}mm,  height: ${height}mm`;
   }
   getAge() {
-    return `${new Date().getFullYear() - this.manufactureDate} years`;
+    return `${
+      new Date().getFullYear() - this.manufactureDate.getFullYear()
+    } years`;
+  }
+  set manufactureDate(value) {
+    if (!(value instanceof Date)) {
+      throw new Error("year must be instans of Date");
+    }
+    this._manufactureDate = value;
+  }
+  get manufactureDate() {
+    return this._manufactureDate;
+  }
+  set model(value) {
+    if (typeof value !== "string") {
+      throw new TypeError("model must be string");
+    }
+    this._model = value;
+  }
+  get model() {
+    return this._model;
   }
 }
 
@@ -41,7 +65,43 @@ class PassengerTransprot extends Vehicle {
   }
   get addPassenger() {
     this.passengersCount++;
-    this.passengersLimit >= this.passengersCount;
+    return this.passengersLimit >= this.passengersCount;
+  }
+
+  set passengersLimit(value) {
+    if (
+      typeof value !== "number" ||
+      Number.isNaN(value) ||
+      !Number.isInteger(value)
+    ) {
+      throw new TypeError("passengersLimit  must be integer number");
+    }
+    if (value < 0) {
+      throw new RangeError("value must be over 0");
+    }
+    this._passengersLimit = value;
+  }
+  get passengersLimit() {
+    return this._passengersLimit;
+  }
+
+  set passengersCount(value) {
+    if (
+      typeof value !== "number" ||
+      Number.isNaN(value) ||
+      !Number.isInteger(value)
+    ) {
+      throw new TypeError("number of passengers must be integer number");
+    }
+    if (value < 0 || value > this.passengersLimit) {
+      throw new RangeError(
+        "value must be over 0 and passengers must be lower then seats on vehicles"
+      );
+    }
+    this._passengersCount = value;
+  }
+  get passengersCount() {
+    return this._passengersCount;
   }
 }
 
@@ -51,9 +111,29 @@ class FreightTransport extends Vehicle {
     this.capacity = capacity;
   }
   checkLoadingPossibility(weight) {
+    if(typeof weight!== "number" ||
+    Number.isNaN(weight)){
+      throw new TypeError("weight must be number");
+    }
+    if (weight <= 0) {
+      throw new RangeError("weight must be greater than 0");
+    }
     return this.capacity >= weight;
   }
+  set capacity(value) {
+    if (
+      typeof value !== "number" ||
+      Number.isNaN(value) ||
+      !Number.isInteger(value)
+    ) {
+      throw new TypeError("capacity must be integer number");
+    }
+    if (value < 0) {
+      throw new RangeError("value must be over 0");
+    }
+    this._capacity = value;
+  }
+  get capacity() {
+    return this._capacity;
+  }
 }
-
-
-
--- js/vehicle.js
+++ js/vehicle.js
@@ -1,33 +1,67 @@
-'use strict'
+"use strict";
 
 const vehicleMaxSize = {
-  length: 42.58,
+  length: 4258,
   width: 1790,
   height: 1492,
 };
 
+const passengerTransprotSize = {
+  length: 7340,
+  width: 1993,
+  height: 3030,
+};
 
-const passangerTransport = new PassengerTransprot(
-  vehicleMaxSize,
-  "geely",
-  "ck2",
-  2018,
-  25,
-  21
-);
+const freightTransportSize = {
+  length: 7370,
+  width: 2180,
+  height: 4400,
+};
 
-const freightTransport = new FreightTransport(
-  vehicleMaxSize,
-  "geely",
-  "ck2",
-  2018,
-  3500
-);
+try {
+  const vehicle = new Vehicle(
+    vehicleMaxSize,
+    "Volkswagen",
+    "Golf",
+    new Date(2016, 0, 12, 12, 34, 23)
+  );
+  console.group("Метод габаритов и возвраста автомобиля");
+  console.log("getMaxSize :>> ", vehicle.getMaxSize());
+  console.log("getAge :>> ", vehicle.getAge());
+  console.groupEnd();
 
-try{
-  const vehicle = new Vehicle(vehicleMaxSize, "Volkswagen", "Golf", 2016);
-  vehicle.getMaxSize();
-  console.log(vehicle.getAge());
-}catch(e){
-  console.log('e :>> ', e);
+  //пасажирский транспорт
+  const passangerTransport = new PassengerTransprot(
+    passengerTransprotSize,
+    "Volkswagen",
+    "Crafter",
+    new Date(2015, 11, 10, 11, 11),
+    27,
+    26
+  );
+
+  console.group("Метод добавления пасажира");
+  console.log('passangerTransport.addPassenger :>> ', passangerTransport.addPassenger);
+  console.groupEnd();
+
+  //грузовой транспорт
+  const freightTransport = new FreightTransport(
+    freightTransportSize,
+    "Hyundai",
+    "Mighty",
+    new Date(2012, 11, 10, 11, 10),
+    5100
+  );
+  console.group("Метод проверки погрузки массы")
+  console.log('freightTransport.checkLoadingPossibility(5100) ', freightTransport.checkLoadingPossibility(5100));
+
+  console.groupEnd()
+} catch (e) {
+  if (e instanceof RangeError) {
+    console.log(e);
+  } else if (e instanceof TypeError) {
+    console.log(e);
+  } else if (e instanceof Error) {
+    console.log(e);
+  }
 }
